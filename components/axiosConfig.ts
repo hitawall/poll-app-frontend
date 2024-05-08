99eb94c44d8baeee3de98b5b0e2f9fe9deb8a30c
shubham.arora <shubhaminkk@gmail.com>
2024-05-08 18:37:51 +0530

    make frontend backend link configurable

--- components/axiosConfig.ts
+++ components/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
@@ -9,16 +9,16 @@ const axiosInstance = axios.create({
     }
 });
 
+// Interceptors remain the same
 axiosInstance.interceptors.request.use(request => {
     console.log("Request URL:", request.url);
-    const publicPaths = ['/login', '/signup']; // List of paths that should not include the token
+    const publicPaths = ['/login', '/signup'];
     const token = localStorage.getItem('token');
     if (token && !publicPaths.includes(request.url)) {
         request.headers['Authorization'] = `Bearer ${token}`;
     }
     return request;
 });
-
 
 axiosInstance.interceptors.response.use(response => response, error => {
     console.error('Response Error:', error);
